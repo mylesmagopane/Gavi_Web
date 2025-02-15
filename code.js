2529696525067be11af485fd905d8a6a8e4baff5
Myles Magopane <mylesm@fhchamberlain.co.za>
2025-02-15 16:13:10 +0200

    first

--- code.js
+++ code.js
@@ -2,6 +2,8 @@
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents")
+var tabIndexes = document.getElementsByClassName("navCont")
+
 
 var typed = new Typed('#typed',{
     strings : ["Crafted for Confidence","Designed for Life","Genuine Leather" +"","Enduring Style","Timeless Quality","Companion for Life."],
@@ -14,14 +16,22 @@ function opentab (tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-link");                          
     }
-
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");                          
     }
-    
    event.currentTarget.classList.add("active-link");   
    document.getElementById(tabname).classList.add("active-tab"); 
 }
+
+
+function selectedIndex (){
+
+    for(tabIndex of tabIndexes){
+        tabIndex.classList.remove("active");                          
+    }
+    event.currentTarget.classList.add("active"); 
+}
+
 
 
 
